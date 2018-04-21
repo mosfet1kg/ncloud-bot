82178f82670bb8210ac9847c220e84d4d9e10889
mosfet1kg <mosfet1kg@gmail.com>
2018-04-22 01:47:02 +0900

    update

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const ncloudApi = require('./helpers/ncloudApi');
     await slackBot.connect();
 
     setInterval(async () => {
-      const changedServerList = await stateStore.set(ncloudApi.getServerInstanceList()
-        .then(response => response.getServerInstanceListResponse.serverInstanceList));
+      const changedServerList = await stateStore.set(
+        ncloudApi.getServerInstanceList()
+          .then(response => response.getServerInstanceListResponse.serverInstanceList)
+      );
 
       changedServerList.forEach(async (serverInfo) => {
         const message = '[서버 상태가 변경되었습니다]\n' +
-        `서버이름: ${ serverInfo.serverName }, 서버상태: ${ serverInfo.serverInstanceStatusName } \n` +
-        `서버이미지: ${ serverInfo.serverImageName }`;
+          `서버이름: ${ serverInfo.serverName }, 서버상태: ${ serverInfo.serverInstanceStatusName } \n` +
+          `서버이미지: ${ serverInfo.serverImageName }`;
 
         await slackBot.postMessageToUser({ username: 'mosfet1kg', message});
       })
